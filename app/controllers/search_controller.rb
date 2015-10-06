486cb15fd07ebe71c95900319e676a07cb354a94
sonu kumar <sonu.kumar@inveera.us>
2015-10-06 17:40:37 +0530

    answer search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   
   def search
-    flash[:alert] = "Please enter a candidate or event name" if params[:q].blank?
-    @events = Event.search(params[:q]).records.to_a
+    flash[:alert] = "Please enter a candidate or event name" if params[:q].blank?&&params[:e].blank?
+    @events = Event.search(params[:e]).records.to_a
     @candidates = Candidate.search(params[:q]).records.to_a    
   end
 end
