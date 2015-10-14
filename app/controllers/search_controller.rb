3e4101509e113ee9da1bb0fe377f1b7f74c9c183
sonu kumar <sonu.kumar@inveera.us>
2015-10-14 19:02:26 +0530

    Minor fixes for searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   
   def search
     flash[:alert] = "Please enter a candidate or event name" if params[:q].blank?&&params[:e].blank?
-    @candidates = params[:e].present? ? Candidate.search(params[:q]).records.to_a : []
+    @candidates = params[:q].present? ? Candidate.search(params[:q]).records.to_a : []
     @events = params[:e].present? ? Event.search(params[:e]).records.to_a : []
     @event_days = params[:d].present? ? Event.search(params[:d]).records.to_a : []
     @venues = params[:v].present? ? Venue.search(params[:v]).records.to_a : []
