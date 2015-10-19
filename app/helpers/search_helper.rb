9d81313b1e1047cb105be7629f0bb07fb6535413
sonu kumar <sonu.kumar@inveera.us>
2015-10-19 18:17:25 +0530

    Removed byebug

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -20,7 +20,6 @@ module SearchHelper
       statement.candidate.person_name.to_s.downcase.include? params[:q] and statement.event_day.event.venue.name.include? params[:v]
     elsif params[:q].present? && params[:d].present?
       statement.candidate.person_name.to_s.downcase.include? params[:q] and statement.event_day.date.strftime("%m/%d/%Y") == params[:d]
-      byebug
     elsif params[:e].present? && params[:v].present? && venue(statement) 
       statement.candidate.events.map(&:title).include? params[:e] and statement.event_day.event.venue.name.include? params[:v]
     elsif params[:e].present? && params[:d].present? && event_day(statement)
@@ -44,7 +43,7 @@ module SearchHelper
   def venue(statement)
     statement.event_day.present? && statement.event_day.event.present? && statement.event_day.event.venue.present?
   end
-  
+
   def event_day(statement)
     statement.event_day.present?
   end
