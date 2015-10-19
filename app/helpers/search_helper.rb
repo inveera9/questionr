4019f454afb1b27966a8492f08c2f3ecdf372b72
sonu kumar <sonu.kumar@inveera.us>
2015-10-19 16:22:00 +0530

    fixed issues of nil class of event day

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -31,9 +31,9 @@ module SearchHelper
       statement.candidate.person_name.to_s.downcase.include? params[:q]
     elsif params[:e].present?
       statement.candidate.events.map(&:title).include? params[:e]
-    elsif params[:v].present?
+    elsif params[:v].present? && statement.event_day.present?
       statement.event_day.event.venue.name.include? params[:v]
-    elsif params[:d].present?
+    elsif params[:d].present? && statement.event_day.present?
       statement.event_day.date.strftime("%m/%d/%Y") == params[:d]
     else
       false
