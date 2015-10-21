914acda77c4a2c8c4a37334ddb0f66bbe9546817
sonu kumar <sonu.kumar@inveera.us>
2015-10-21 18:22:38 +0530

    Disable the link of candidate & remove result, public on search & enable the video

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,6 +5,7 @@ class SearchController < ApplicationController
     if params[:q].blank? && params[:e].blank? && params[:v].blank? && params[:d].blank?
       flash[:alert] = "Please fill at least one field." 
     else
+      flash[:alert] = nil
       @statements = Statement.advance_search(params[:q],params[:v],params[:d],params[:e])
     end
   end
