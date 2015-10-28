251c4f8b70148aa95444aae5c4b036bc7addc74b
sonu kumar <sonu.kumar@inveera.us>
2015-10-28 11:48:43 +0530

    Tagging & searching result shows the videos

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
   
   def search
-    @statements = []
-    @statements = Statement.advance_search(params[:q],params[:v],params[:d],params[:e])
-    @statements = Statement.tag_search(params[:t]) if params[:t].present?
+    @videos = []
+    @videos = Statement.advance_search(params[:q],params[:v],params[:d],params[:e])
+    @videos = Statement.tag_search(params[:t]) if params[:t].present?
   end
 end
 
