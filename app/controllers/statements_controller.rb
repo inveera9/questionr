f8474eae528858cacceffffff5467ff5f332099a
sonu kumar <sonu.kumar@inveera.us>
2015-11-04 15:41:00 +0530

    Transcriptin functionality implemented

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -97,6 +97,7 @@ class StatementsController < ApplicationController
                                         :ugc_notes, 
                                         :youtube_url,
                                         :third_party_url,
+                                        :transcript,
                                         :tag_list,
                                         user_attributes: [:first_name, :last_name, :email, :mobile_phone, :postal_code])
     end
