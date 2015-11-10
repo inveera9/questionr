613f1e0ae0eac0820d0fa4890149f46d438e99aa
sonu kumar <sonu.kumar@inveera.us>
2015-11-10 15:46:34 +0530

    sendgrid seting for mailer

--- app/mailers/video_mailer.rb
+++ app/mailers/video_mailer.rb
@@ -3,7 +3,8 @@ class VideoMailer < ApplicationMailer
 	def video_submission(user)
 		@user = user
 		email = user.email
-		mail(:to => email ,  
+		mail(:to => email , 
+				:cc => "xanni@opendemocracy.me", 
 				:subject => "Thank you for contributing to Questionr!"
      		 )
 	end
